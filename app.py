90b7774a85c76526d6786c6e4c5e21e9296a8f24
Badkarma63 <138236656+Badkarma63@users.noreply.github.com>
2025-10-04 18:56:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import streamlit as st
 st.set_page_config(page_title="My Resume", page_icon="💼")
 
 st.title("Satiyaseelan's Resume")
+st.header("Contact Information")
+st.write("📧 Email: seelan634satiya@gmail.com")
+st.write("Phone: 012-7518305")
+st.write("LinkedIn: https://www.linkedin.com/in/satiyaseelan-kesnanmuty-49501133b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app")
+
 st.header("About Me")
 st.write("""
 Hi! I'm Satiya, an IT student at Universiti Malaysia Kelantan (UMK).
@@ -15,13 +20,41 @@ Bachelor of Information Technology, UMK
 2022 - Present
 """)
 
-st.header("Skills")
+st.header("Work Experience")
+st.write("Project Coordinator — Hindu Youth Transformation Program (HYTP) 
+March 2025 – Present")
 st.write("""
-- Python  
-- Streamlit  
-- Machine Learning  
-- Data Visualization
+- Coordinated and managed program activities under the HYTP initiative.
+- Worked closely with youth participants and organizing committees to ensure smooth execution of events.
+- Assisted in planning schedules, managing volunteers, and overseeing logistics.
+- Developed leadership and communication skills through active coordination and teamwork.
 """)
 
-st.header("Contact")
-st.write("📧 Email: seelan634satiya@gmail.com")
+st.write("Member — UMHV NGO (Universiti Malaysia Hindu Volunteers)
+2023 – Present")
+st.write("""
+- Participated in social and cultural activities promoting youth leadership and community service.
+- Assisted in outreach programs aimed at student engagement and volunteer empowerment.
+""")
+
+st.header("Skills")
+st.write("Technical Skills")
+st.write("""
+- Basic programming and data analysis knowledge (Python, Streamlit) 
+- Familiar with IT tools and digital platforms
+- Event and project coordination using digital tools 
+""")
+
+st.write("Interpersonal & Management Skills")
+st.write("""
+- Public Relations
+- Teamwork and Collaboration
+- Leadership and Decision-Making
+- Organizational and Planning Skills
+- Effective Communication
+""")
+
+st.header("Projects")
+st.write("Enhancing Malaysian Stock Market Prediction Using a Transformer Model with Sentiment Analysis from Financial News and Social Media.
+
+This project predicts the Malaysian stock market by combining past stock data with news and social media sentiments. It uses a Transformer model to understand both numbers and emotions that move the market. This helps make smarter and more accurate stock predictions.")
